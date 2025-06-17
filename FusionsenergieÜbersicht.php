1bb26067d1fe47bc510040b818e36ef43c06307e
Jannik Muy <Jak250102@gmail.com>
2025-06-18 00:16:22 +0200

    bla

--- FusionsenergieÜbersicht.php
+++ FusionsenergieÜbersicht.php
@@ -1,5 +1,5 @@
 <?php
-include("/functions.php");
+include __DIR__ . '/functions.php';
 printHeader("Übersicht Fusionsenergie");
 includeIfExists("/HTML/AS Übersicht.html");
 includeIfExists("/HTML/footer.html");
